3d4f020f3f022fe72034381b57de5c62cf1f6fc9
Siddhesh Pansare <61089801+Siddhesh-hub@users.noreply.github.com>
2023-03-22 19:45:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas as pd
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
@@ -9,8 +13,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
-import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -26,7 +28,6 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 #take the json version of the response and normalize it
@@ -34,17 +35,25 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 #output it the screen as table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+#font run anthing past here while we trouble shoot 
+streamlit.stop()
+
+
+
+
+
+#Let's put a pick list here so het can pick the fruit they want to include
+add_my_fruit = streamlit.multiselect("Which fruit would you like to add?", list(my_data_rows))
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-#Let's put a pick list here so het can pick the fruit they want to include
-add_my_fruit = streamlit.multiselect("Which fruit would you like to add?", list(my_data_rows))
-streamlit.write('Thanks for adding', add_my_fruit)
+fruit_advice_to_show = my_fruit_list.loc[add_my_fruit]
+
+#Display the table on the page
+streamlit.dataframe(fruit_advice_to_show)
 
 my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
 
